5ccd55efa23470d9e736f9e8f23be971ce4cbf92
Bradley <40966735+BradleyNewell@users.noreply.github.com>
2021-06-11 07:30:43 +0100

    Expanded to class and added pie chart
    - Removed base function
    
    - Added new class to encapsulate application
    
    - Added pie chart for improved accessibility
    
    - Imported matplotlib for pie chart
    
    -  Added method to generate automatic percentage for matplotlib labelling
    
    - Added makeshift test instantiation statements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import shutil
+import matplotlib.pyplot as plt
 
 """
 The show_disc_info() function takes a path of a drive 
@@ -7,11 +8,45 @@ on the specified drive.
 """
 
 
-def show_disc_info(path):
-    total, used, free = shutil.disk_usage(path)
-    print(f"Total: {total // (2**30)} gb on drive: {str(path)}")
-    print(f"Used: {used // (2**30)} gb on drive:  {str(path)}")
-    print(f"Free: {free // (2**30)} gb on drive:  {str(path)}")
+class StorageReport:
+
+    def __init__(self, drive_path):
+        self.drive_path = drive_path
+        self.total, self.used, self.free = shutil.disk_usage(drive_path)
+        self.formatted_used = self.used // (2**30)
+        self.formatted_free = self.free // (2**30)
+        self.disc_values = [self.formatted_used, self.formatted_free]
+
+    def show_disc_info(self):
+        print(f"Total: {self.total // (2**30)} gb on drive: {str(self.drive_path)}")
+        print(f"Used: {self.formatted_used} gb on drive:  {str(self.drive_path)}")
+        print(f"Free: {self.formatted_free} gb on drive:  {str(self.drive_path)}")
+
+    # Used to display the percent values and original values on the pie chart
+
+    def generate_autopct(self, d_values):
+        def make_autopct(pct):
+            total = sum(d_values)
+            val = int(round(pct*total/100.0))
+            return f'{pct:.2f}% ({val:d}gb)'
+        return make_autopct
+
+    def display_disc_graph(self):
+        colours = ['#ff9999', '#99ff99']
+        disc_info = 'Used', 'Free'
+        explode_pie = (0.05, 0.05)
+        plt.pie(self.disc_values,
+                autopct=self.generate_autopct(self.disc_values),
+                colors=colours,
+                pctdistance=0.85,
+                explode=explode_pie,
+                shadow=True
+                )
+        plt.legend(disc_info, loc="best")
+        plt.show()
 
 
-show_disc_info("C:")
+
+x = StorageReport("C:")
+x.show_disc_info()
+x.display_disc_graph()
